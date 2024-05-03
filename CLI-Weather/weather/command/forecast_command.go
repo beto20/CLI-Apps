e7a2869da65f65bf5b6324aa0972f97b70dc631c
Alberto Velasquez <alberto.vs@outlook.com>
2024-05-03 00:52:35 -0500

    fix: forecast feature

--- CLI-Weather/weather/command/forecast_command.go
+++ CLI-Weather/weather/command/forecast_command.go
@@ -32,11 +32,18 @@ func (fc *ForecastCommand) GetForecastCommand(arg string) {
 	nw := service.NewWeather()
 	forecasts := nw.GetForecast(arg)
 
-	var fcdetails = ForecastCommand{}
+	var fcDetails []struct {
+				Date           string
+				MaxTempCelsius float64
+				MinTempCelsius float64
+				AvgTempCelsius float64
+				AvgHumidity    float64
+				Sunrise        string
+				Sunset         string
+  }
 
 	for _, fc := range forecasts.Forecast.Forecastday {
-		x := ForecastCommand{
-			ForecastDetails: []struct {
+		x := struct {
 				Date           string
 				MaxTempCelsius float64
 				MinTempCelsius float64
@@ -45,26 +52,38 @@ func (fc *ForecastCommand) GetForecastCommand(arg string) {
 				Sunrise        string
 				Sunset         string
 			}{
-				{Date: fc.Date},
-				{MaxTempCelsius: fc.Day.MaxTempCelsius},
-				{MinTempCelsius: fc.Day.MinTempCelsius},
-				{AvgTempCelsius: fc.Day.AvgTempCelsius},
-				{AvgHumidity: fc.Day.AvgHumidity},
-				{Sunrise: fc.Astro.Sunrise},
-				{Sunset: fc.Astro.Sunset},
-			},
-		}
+				Date: fc.Date,
+				MaxTempCelsius: fc.Day.MaxTempCelsius,
+				MinTempCelsius: fc.Day.MinTempCelsius,
+				AvgTempCelsius: fc.Day.AvgTempCelsius,
+				AvgHumidity: fc.Day.AvgHumidity,
+				Sunrise: fc.Astro.Sunrise,
+				Sunset: fc.Astro.Sunset,
+      }
 
-		fcdetails.ForecastDetails = append(fcdetails.ForecastDetails, x.ForecastDetails...)
+    fcDetails = append(fcDetails, x)
 	}
 
-	y := ForecastCommand{
-		Name:            forecasts.Location.Name,
-		Country:         forecasts.Location.Country,
-		ForecastDetails: fcdetails.ForecastDetails,
-	}
+  y := ForecastCommand {
+    Name: forecasts.Location.Name,
+    Country: forecasts.Location.Country,
+    ForecastDetails: fcDetails,
+  }
 
-	fmt.Println("0: ", y.ForecastDetails[0].Sunset)
-	fmt.Println("1: ", y.ForecastDetails[1].Sunset)
-	fmt.Println("2: ", y.ForecastDetails[2].Sunset)
+	fmt.Println("0: ", y.Name)
+	fmt.Println("0: ", y.Country)
+
+  
+  fmt.Println("1a: ", y.ForecastDetails[0].Date)
+  fmt.Println("1b: ", y.ForecastDetails[1].Date)
+  fmt.Println("1c: ", y.ForecastDetails[2].Date)
+
+  fmt.Println("2: ", y.ForecastDetails[1].MaxTempCelsius)
+  fmt.Println("3: ", y.ForecastDetails[1].MinTempCelsius)
+  fmt.Println("4: ", y.ForecastDetails[1].Sunrise)
+
+	fmt.Println("5: ", y.ForecastDetails[0].Sunset)
+	fmt.Println("5: ", y.ForecastDetails[1].Sunset)
+	fmt.Println("5: ", y.ForecastDetails[2].Sunset)
 }
+
